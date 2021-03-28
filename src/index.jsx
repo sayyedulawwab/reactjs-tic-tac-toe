8937fb1e682676b103da055cad5d8288185c2564
sayyedulbappy <38081373+sayyedulbappy@users.noreply.github.com>
2021-03-28 13:17:33 +0600

    separate the components

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+
+import ReactDOM from 'react-dom';
+import './index.css';
+import { Game } from "./components/Game";
+
+
+
+
+// ========================================
+
+ReactDOM.render(
+  <Game />,
+  document.getElementById('root')
+);
+
